30248483431bc934e1e1a9deec8b6821ba051e56
Benita Besa <benita.besa@duke.edu>
2025-04-10 17:01:55 -0400

    Initial Commit

---
+++ SyntaxFound/SyntaxFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyntaxFoundApp.swift
+//  SyntaxFound
+//
+//  Created by Benita Besa on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SyntaxFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SyntaxFoundTests/SyntaxFoundTests.swift
@@ -0,0 +1,16 @@
+//
+//  SyntaxFoundTests.swift
+//  SyntaxFoundTests
+//
+//  Created by Benita Besa on 4/10/25.
+//
+
+import Testing
+
+struct SyntaxFoundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
